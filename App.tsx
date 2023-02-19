e875b62f6ea8fe3b7877be74407b65967baeb70b
Will Worthan <worthanwill@gmail.com>
2023-02-18 23:04:57 -0500

    added previous group's assets, React Navigation boilerplate code, and a sample splash screen that appears when the app is run

--- App.tsx
+++ App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import { View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
   return (
-    <View
-      style={{
+    <NavigationContainer>
+      <View
+        style={{
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-      }}>
-      <Text>Hello, world!</Text>
-    </View>
+        }}>
+        <Image
+            source={require('./src/assets/studio-buddy-logo.png')}
+            style={{width: '90%', resizeMode: 'contain', margin: 30}}
+            />
+      </View>
+    </NavigationContainer>
   );
 };
 export default App;
